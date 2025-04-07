fdf7ad17925ac57c8b3bfa969c94bfd4c6fc2fce
Shreya Patel <eya672004@gmail.com>
2025-04-06 22:02:18 -0400

    set up the django project

--- src/posts_proj/urls.py
+++ src/posts_proj/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from djago.conf import settings
+from djago.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL_URL, document_root=settings.MEDIA_ROOT)
