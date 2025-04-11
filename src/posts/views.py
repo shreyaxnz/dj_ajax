4261e985aea4b9cb00b28fef15e9c2dc6f409223
Shreya Patel <eya672004@gmail.com>
2025-04-11 18:02:49 -0400

    added login required everywhere

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,8 +4,10 @@ from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 from .utils import action_permission
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def post_list_and_create(request):
     form = PostForm(request.POST or None)
     #qs = Post.objects.all()
@@ -27,6 +29,7 @@ def post_list_and_create(request):
     }
     return render(request,'posts/main.html', context)
 
+@login_required
 def post_detail(request, pk):
     obj = Post.objects.get(pk=pk)
     form = PostForm()
@@ -38,6 +41,7 @@ def post_detail(request, pk):
 
     return render(request, 'posts/detail.html',context)
 
+@login_required
 def load_post_data_view(request, num_posts):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         visible = 3
@@ -59,6 +63,7 @@ def load_post_data_view(request, num_posts):
             data.append(item)
         return JsonResponse({'data':data[lower:upper],'size': size})
 
+@login_required
 def post_detail_data_view(request, pk):
     obj = Post.objects.get(pk=pk)
     data = {
@@ -70,6 +75,7 @@ def post_detail_data_view(request, pk):
     }
     return JsonResponse({'data':data})
 
+@login_required
 def like_unlike_post(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         pk = request.POST.get('pk')
@@ -82,6 +88,8 @@ def like_unlike_post(request):
             obj.liked.add(request.user)
         return JsonResponse({'liked':liked, 'count': obj.like_count})
 
+@login_required
+@action_permission
 def update_post(request,pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -95,6 +103,7 @@ def update_post(request,pk):
             'body': new_body,
         })
 
+@login_required
 @action_permission
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -49,6 +49,8 @@ CRISPY_ALLOWED_TEMPLATE_PACK = ''
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_URL = '/admin/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
