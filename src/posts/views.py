f89bac0b3a0b87b6220541ba80523c0d75913ecb
Shreya Patel <eya672004@gmail.com>
2025-04-11 16:28:40 -0400

    IMproved views

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,6 +3,7 @@ from .models import Post, Photo
 from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
+from .utils import action_permission
 # Create your views here.
 
 def post_list_and_create(request):
@@ -94,11 +95,13 @@ def update_post(request,pk):
             'body': new_body,
         })
 
+@action_permission
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         obj.delete()
-    return JsonResponse({})
+        return JsonResponse({'msg':'some message'})
+    return JsonResponse({'msg':'access denied -ajax only'})
 
 def image_upload_view(request):
     if request.method == 'POST':
