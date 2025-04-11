e24fc160c6b07076188b9095a7dc2f1f9e97c02c
Shreya Patel <eya672004@gmail.com>
2025-04-11 15:03:54 -0400

    Created user profile

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Profile
+from .forms import ProfileForm
+from .django.http import JsonResponse
 # Create your views here.
+
+def my_profile_view(request):
+    obj = Profile.objects.get(user=request.user)
+    form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
+    
