52664c567f38156a3d5ad46cabac99f8a6078d3c
Shreya Patel <eya672004@gmail.com>
2025-04-11 10:12:30 -0400

    Added back button, removed spinner on details page

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,7 +3,8 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    post_detail
+    post_detail,
+    post_detail_data_view
 
 )
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path('<pk>/',post_detail, name='post-detail'),
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/',post_detail_data_view, name='post-detail-data-view'),
 ]
