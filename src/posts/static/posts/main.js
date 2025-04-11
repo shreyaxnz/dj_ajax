6d24f46d0de85a1169814e1f4309a61b80f64c64
Shreya Patel <eya672004@gmail.com>
2025-04-11 14:52:47 -0400

    Did some improvements

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -179,6 +179,8 @@ closeBtns.forEach(btn=> btn.addEventListener('click',()=>{
     if (!dropzone.classList.contains('not-visible')){
         dropzone.classList.add('not-visible')
     }
+    const myDropzone = Dropzone.forElement("#my-dropzone")
+    myDropzone.removeAllFiles(true)
 }))
 
 Dropzone.autoDiscover = false
@@ -190,7 +192,7 @@ const myDropzone = new Dropzone('#my-dropzone',{
             formData.append('new_post_id',newPostId)
         })
     },
-    maxFiles: 5,
+    maxFiles: 3,
     maxFilesize: 4,
     acceptedFiles: '.png, .jpg, .jpeg'
 })
